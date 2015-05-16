5eeb735c9657057937d189a03a9cdd5c9041bb30
alex9311 <alex.simes29@gmail.com>
2015-05-16 12:18:14 +0200

    added to form

--- apps/movies/movie_form.php
+++ apps/movies/movie_form.php
@@ -1,9 +1,27 @@
+<h2>Movie Query Machine</h2>
+<?php
+	$genre_response = file_get_contents('http://localhost:8080/exist/rest/db/movies?_query=/movies//genre');
+  	$genres = simplexml_load_string($genre_response);
+?>
+
 <html>
 <body>
 
 <form action="get_movies.php" method="post">
-	Name: <input type="text" name="title"><br>
-	E-mail: <input type="text" name="email"><br>
+	Movie Tile: <input type="text" name="title"><br>
+	Genre:
+<select>
+	<option value=""></option>
+	<?php
+		foreach($genres->genre as $genre){
+			echo '<option value="'.$genre.'">'.$genre.'</option>';
+		}
+	?>
+</select><br>
+	Director: <input type="text" name="email"><br>
+	Actor: <input type="text" name="email"><br>
+	Year: <input type="text" name="email"><br>
+	Key Words: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
