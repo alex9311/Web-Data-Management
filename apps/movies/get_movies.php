08135e186c8ab697c8d48350214d5cd2028b03f3
alex9311 <alex.simes29@gmail.com>
2015-05-16 12:02:23 +0200

    can now query movie based on title

---
+++ apps/movies/get_movies.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$title = $_POST["title"];
+	$query = 'http://localhost:8080/exist/rest/db/movies?_query=/movies/movie[title="'.$title.'"]/node()';
+	echo "<b>Query: </b>".$query."<br><br>";
+	$response = file_get_contents('http://localhost:8080/exist/rest/db/movies?_query=/movies/movie[title="'.$title.'"]/node()');
+	$xml = simplexml_load_string($response);
+	echo "<b>Response in array format:</b> <br>";
+	print_r($xml);
+
+	echo '<br><br><a href="movie_form.php"> Try another query!</a>';
+}
+
+?>
--- apps/movies/movie_form.php
+++ apps/movies/movie_form.php
@@ -1,13 +1,9 @@
-<?php
-$response = file_get_contents('http://localhost:8080/exist/rest/db/movies/movies.xml');
-echo $response
-?>
 <html>
 <body>
 
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+<form action="get_movies.php" method="post">
+	Name: <input type="text" name="title"><br>
+	E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
