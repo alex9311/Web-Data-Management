e495d09d5b913d819306346b829df827f73c144b
PetervB <petervanbuul@gmail.com>
2015-05-24 21:00:53 +0200

    Removed comment

--- apps/music/upload.php
+++ apps/music/upload.php
@@ -11,17 +11,7 @@ $target_dir = "xml/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-//if(isset($_POST["submit"])) {
-//    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//    if($check !== false) {
-//        echo "File is an image - " . $check["mime"] . ".";
-//        $uploadOk = 1;
-//    } else {
-//        echo "File is not an image.";
-//        $uploadOk = 0;
-//    }
-//}
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
