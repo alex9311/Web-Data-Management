d4ac1fea92b89c8cafb9036f5462b140e79b67a9
alex9311 <alex.simes29@gmail.com>
2015-06-11 12:27:23 +0200

    some restructuring, working on edit

---
+++ Assignment2/app/handle_edit.php
@@ -0,0 +1,45 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	print_r($_POST);
+	$title = $_POST["title"];
+	$type = $_POST["type"];
+	$year = $_POST["year"];
+	$publisher = $_POST["publisher"];
+	$source = $_POST["source"];
+	$author = $_POST["author"];
+	$id = $_POST["id"];
+	$rev = $_POST["rev"];
+	
+	$ch = curl_init();
+ 
+	$customer = array(
+		'title' => $title,
+		'type' => $type,
+		'year' => $year,
+		'publisher' => $publisher,
+		'source' => $source,
+		'author' => $author,
+	);
+	 
+	$customer['_rev'] = $rev;
+ 
+	$payload = json_encode($customer);
+ 
+	curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:5984/books/'.$id);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); /* or PUT */
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-type: application/json',
+		'Accept: */*'
+	));
+ 
+	curl_setopt($ch, CURLOPT_USERPWD, 'admin:admin');
+ 
+	$response = curl_exec($ch);
+ 
+	curl_close($ch); 
+}
+
+?>
