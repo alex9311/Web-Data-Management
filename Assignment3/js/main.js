a02db7b0d2dacdaf04469da680dfcd8faaaf1129
alex9311 <alex.simes29@gmail.com>
2015-06-19 17:20:45 +0200

    changed folder name

--- Assignment3/js/main.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp', []);
-
-app.controller('MyController', function($scope) {
-	xdmp.httpGet("http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk");
-	
-	$scope.person = {
-		name: results
-	};
-});
