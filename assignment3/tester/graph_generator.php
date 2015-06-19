6e53fdbab448ca0c85576ab5e8d7e0d3e426fce1
alex9311 <alex.simes29@gmail.com>
2015-06-19 17:46:40 +0200

    restructuring directories

---
+++ assignment3/tester/graph_generator.php
@@ -0,0 +1,34 @@
+<?php
+
+if(count($argv)!=2){
+	echo "please pass the number of nodes as an argument\n";
+	exit();
+}elseif(!is_numeric($argv[1])){
+	echo "please pass the number of nodes as an argument\n";
+	exit();
+}
+	
+
+$num_nodes = $argv[1];
+$nodes = range(0,$num_nodes-1);
+
+for ($x = 0; $x < $num_nodes; $x++) {
+	echo "for node number $x\n";
+	generate_edges($x,$nodes);
+	echo "\n";
+
+} 
+
+function generate_edges($node_num,$nodes){
+	unset($nodes[$node_num]);
+	$possible_connections = array_values($nodes);
+	$num_connections = rand(1,count($nodes));
+
+	shuffle($possible_connections);
+	$connected_nodes = array_slice($possible_connections,0,$num_connections);
+
+	echo "connections:\n";
+	print_r(implode(" ",$connected_nodes));echo "\n";
+}
+
+?>
